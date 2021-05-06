f94f7f9721a5da905fb035876cd27505f32ae8c7
codingMasterHari <80979736+codingMasterHari@users.noreply.github.com>
2021-05-06 13:35:23 -0400

    Add files via upload

---
+++ js/launcher.js
@@ -0,0 +1,27 @@
+class Launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.004,
+            length: 10
+        }
+        this.pointB = pointB;
+        this.launcherObject = Constraint.create(options);
+        World.add(world, this.launcherObject);
+    }
+  
+    fly(){
+        this.launcherObject.bodyA = null;
+    }
+  
+   display(){
+        if(this.launcherObject.bodyA){
+            var pointA = this.launcherObject.bodyA.position;
+            var pointB = this.pointB;
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+        strokeWeight(4);
+        stroke("black");
+    }
+}
